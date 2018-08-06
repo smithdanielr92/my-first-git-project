43af1048220299ebd6837b815e68d54f520f40bb
Daniel Smith <36824195+smithdanielr92@users.noreply.github.com>
2018-08-06 08:15:32 -0400

    E X P A N D E D

--- index.js
+++ index.js
@@ -1,42 +1,111 @@
 const rp = require("request-promise");
 const options = {
     method: "GET",
-    url: "https://us.api.battle.net/wow/character/stormrage/Aeronautics?locale=en_US&fields=statistics&apikey=4zhp9gcunyhhedfye3bcypg698chch9j"
+    url: "https://us.api.battle.net/wow/character/stormrage/Busy?locale=en_US&fields=statistics&apikey=4zhp9gcunyhhedfye3bcypg698chch9j"
 };
-rp(options)
-.then(data => {
+
+function getBgs(stats) {
+    let bgInfo = {};
+    bgInfo.av = {
+        played: stats.statistics.subCategories[9].subCategories[1].statistics[4].quantity,
+        won: stats.statistics.subCategories[9].subCategories[1].statistics[5].quantity
+    };
+    bgInfo.ab = {
+        played: stats.statistics.subCategories[9].subCategories[1].statistics[6].quantity,
+        won: stats.statistics.subCategories[9].subCategories[1].statistics[7].quantity
+    }
+    bgInfo.bfg = {
+        played: stats.statistics.subCategories[9].subCategories[1].statistics[8].quantity,
+        won: stats.statistics.subCategories[9].subCategories[1].statistics[9].quantity
+    }
+    bgInfo.eots = {
+        played: stats.statistics.subCategories[9].subCategories[1].statistics[10].quantity,
+        won: stats.statistics.subCategories[9].subCategories[1].statistics[12].quantity
+    }
+    bgInfo.ss = {
+        played: stats.statistics.subCategories[9].subCategories[1].statistics[13].quantity,
+        won: stats.statistics.subCategories[9].subCategories[1].statistics[14].quantity
+    }
+    bgInfo.sota = {
+        played: stats.statistics.subCategories[9].subCategories[1].statistics[15].quantity,
+        won: stats.statistics.subCategories[9].subCategories[1].statistics[16].quantity
+    }
+    bgInfo.tp = {
+        played: stats.statistics.subCategories[9].subCategories[1].statistics[17].quantity,
+        won: stats.statistics.subCategories[9].subCategories[1].statistics[18].quantity
+    }
+    bgInfo.wsg = {
+        played: stats.statistics.subCategories[9].subCategories[1].statistics[19].quantity,
+        won: stats.statistics.subCategories[9].subCategories[1].statistics[20].quantity
+    }
+    bgInfo.sm = {
+        played: stats.statistics.subCategories[9].subCategories[1].statistics[21].quantity,
+        won: stats.statistics.subCategories[9].subCategories[1].statistics[22].quantity
+    }
+    bgInfo.tok = {
+        played: stats.statistics.subCategories[9].subCategories[1].statistics[23].quantity,
+        won: stats.statistics.subCategories[9].subCategories[1].statistics[24].quantity
+    }
+    bgInfo.ioc = {
+        played: stats.statistics.subCategories[9].subCategories[1].statistics[25].quantity,
+        won: stats.statistics.subCategories[9].subCategories[1].statistics[26].quantity
+    }
+    bgInfo.dwg = {
+        played: stats.statistics.subCategories[9].subCategories[1].statistics[28].quantity,
+        won: stats.statistics.subCategories[9].subCategories[1].statistics[27].quantity
+    }
+    return bgInfo;
+}
+
+rp(options).then(data => {
     const stats = JSON.parse(data);
-    let kd = {};
-    kd.kills = {};
-    kd.deaths = {};
+    
+    let bgsPlayed = stats.statistics.subCategories[9].subCategories[1].statistics[0].quantity;
+    let bgsWon = stats.statistics.subCategories[9].subCategories[1].statistics[2].quantity;
     let killIndex = 9;
     let deathIndex = 0;
     let bgs = ["Alterac Valley", "Arathi Basin", "Warsong Gulch", "Eye of the Storm", "Strand of the Ancients", "Twin Peaks", "The Battle for Gilneas", "Silvershard Mines", "Temple of Kotmogu", "Deepwind Gorge"];
+    let abbreviations = ["av", "ab", "wsg", "eots", "sota", "tp", "bfg", "sm", "tok", "dwg"];
+    let bgInfo = getBgs(stats);
+    
+
     let bgIndex = 0;
     let totalKills = 0;
     let totalDeaths = 0;
-
-    console.log("PvP statistics for " + stats.name + " on " + stats.realm); 
-    while(bgIndex < bgs.length) {
-        if(killIndex === 14) {
-            killIndex ++; // Skip Isle of Conquest kills
+    console.log("---PvP statistics for " + stats.name + " on " + stats.realm + "---\n");
+    while (bgIndex < bgs.length) {
+        if (killIndex === 14) {
+            killIndex++; // Skip Isle of Conquest kills
         }
-        bgKill = stats.statistics.subCategories[2].subCategories[2].statistics[killIndex].quantity;
-        bgDeath = stats.statistics.subCategories[3].subCategories[1].statistics[deathIndex].quantity;
+        let bgKill = stats.statistics.subCategories[2].subCategories[2].statistics[killIndex].quantity;
+        let bgDeath = stats.statistics.subCategories[3].subCategories[1].statistics[deathIndex].quantity;
         let ratio = (bgKill !== 0 || bgDeath !== 0) ? (bgKill / bgDeath).toFixed(2) : "N/A";
         totalKills += bgKill;
         totalDeaths += bgDeath;
+        let bgPlayed = bgInfo[abbreviations[bgIndex]].played;
+        let bgWon = bgInfo[abbreviations[bgIndex]].won;
+        let bgWinRate = ((bgWon / bgPlayed) * 100).toFixed(2);
+        console.log(bgs[bgIndex] + " games played: " + bgPlayed);
+        console.log(bgs[bgIndex] + " games won: " + bgWon);
+        console.log(bgs[bgIndex] + " win rate: " + bgWinRate + "%");
         console.log("K/D in " + bgs[bgIndex] + ": " + bgKill + "/" + bgDeath);
+        console.log("Average kills per " + bgs[bgIndex] + " game: " + (bgKill / bgPlayed).toFixed(2));
+        console.log("Average deaths per " + bgs[bgIndex] + " game: " + (bgDeath / bgPlayed).toFixed(2));
         console.log("Ratio: " + ratio);
+        console.log("\n");
         killIndex++;
         deathIndex++;
         bgIndex++;
     }
     let totalRatio = (totalKills !== 0 || totalDeaths !== 0) ? (totalKills / totalDeaths).toFixed(2) : "N/A";
+    console.log("---TOTALS---");
+    console.log("Battlegrounds played: " + bgsPlayed);
+    console.log("Battlegrounds won: " + bgsWon);
+    console.log("Battlegrounds win rate: " + (Number.parseFloat((bgsWon / bgsPlayed).toFixed(4)) * 100) + "%");
     console.log("TOTAL KILLS: " + totalKills);
     console.log("TOTAL DEATHS: " + totalDeaths);
     console.log("RATIO: " + totalRatio);
-    
+
     let arenaWins = stats.statistics.subCategories[9].subCategories[0].statistics[0].quantity;
     let arenasPlayed = stats.statistics.subCategories[9].subCategories[0].statistics[1].quantity;
     let arenaWL = (arenasPlayed !== 0) ? (arenaWins / arenasPlayed).toFixed(3) : "N/A";
@@ -55,28 +124,30 @@ rp(options)
     console.log("---ARENAS---");
     console.log("Arena wins: " + arenaWins);
     console.log("Arenas played: " + arenasPlayed);
-    console.log("Arena W/L: " + arenaWL);
+    console.log("Arena Win Percentage: " + arenaWL);
     console.log("-----");
     console.log("2v2 wins: " + twosWins);
     console.log("2v2 matches: " + twosPlayed);
-    console.log("2v2 W/L: " + twosWL);
+    console.log("2v2 Win Percentage: " + twosWL);
     console.log("2v2 best personal rating: " + twosRating);
     console.log("-----");
     console.log("3v3 wins: " + threesWins);
     console.log("3v3 matches: " + threesPlayed);
-    console.log("3v3 W/L: " + threesWL);
+    console.log("3v3 Win Percentage: " + threesWL);
     console.log("3v3 best personal rating: " + threesRating);
     console.log("-----");
     console.log("5v5 wins: " + fivesWins);
     console.log("5v5 matches: " + fivesPlayed);
-    console.log("5v5 W/L: " + fivesWL);
+    console.log("5v5 Win Percentage: " + fivesWL);
     console.log("5v5 best personal rating: " + fivesRating);
     console.log("---DUELS---");
 
     let duelsWon = stats.statistics.subCategories[9].subCategories[2].statistics[0].quantity
     let duelsLost = stats.statistics.subCategories[9].subCategories[2].statistics[1].quantity
-    let duelWL = (duelsWon !== 0 || duelsLost !== 0) ? (duelsWon / duelsLost).toFixed(2) : "N.A";
+    let duelWL = (duelsWon !== 0 || duelsLost !== 0) ? Number.parseFloat(duelsWon / (duelsWon + duelsLost)).toFixed(4) * 100 : "N.A";
     console.log(stats.statistics.subCategories[9].subCategories[2].statistics[0].name + ": " + duelsWon);
     console.log(stats.statistics.subCategories[9].subCategories[2].statistics[1].name + ": " + duelsLost);
-    console.log("Duel W/L: " + duelWL);
+    console.log("Duel Win Percentage: " + duelWL + "%");
 });
+
+
